58ec9bc55922857962f5dc06a61d2b93c69d776c
steve <dev@somecool.net>
2020-07-06 00:24:55 +0000

    minor doc clarifications

--- keepers/prompt/src/term/mod.rs
+++ keepers/prompt/src/term/mod.rs
@@ -1,13 +1,14 @@
-//
-//
+//! functions for prompting user to enter text (hidden & non-hidden)
+
 use rpassword::prompt_password_stderr;
 use std::io::{self, Write};
 
-/// Get password from terminal.
-/// If password is non-empty, returns Some(p), otherwise, prints error message and returns None.
+/// Prompt the user to enter a password. Characters are masked (not echoed to terminal).
+/// If password is non-empty, returns `Some(p)``,
+/// otherwise, prints an error message to the console and returns `None`.
 ///
-/// Single prompt- for use with existing vaults. For new password entry, use
-/// get_password_with_confirm.
+/// This function does not prompt for confirmation, so for new keys, use
+/// `get_password_with_confirmation` instead.
 pub fn get_password(prompt: &str) -> Option<String> {
     if let Ok(p) = prompt_password_stderr(prompt) {
         if p.len() > 0 {
@@ -18,9 +19,10 @@ pub fn get_password(prompt: &str) -> Option<String> {
     None
 }
 
-/// Get password from terminal. Prompts for confirmation.
-/// If password is non-empty and confirmation matches, returns Some(p),
-/// otherwise, prints error message and returns None.
+/// Prompt the user to enter a password, and prompt again for confirmation.
+/// If the passwords match and are non-empty, returns `Some(p)`,
+/// otherwise, prints an error to the console and returns `None`.
+/// Characters typed are masked (not echoed to terminal).
 pub fn get_password_with_confirm(prompt: &str, confirm_prompt: &str) -> Option<String> {
     if let Some(p1) = get_password(prompt) {
         if let Some(p2) = get_password(confirm_prompt) {
@@ -35,8 +37,8 @@ pub fn get_password_with_confirm(prompt: &str, confirm_prompt: &str) -> Option<S
     None
 }
 
-/// Get a user input string with prompt.
-/// Unlike get_password* functions, this does not mask typed chars.
+/// Prompts the user to enter a string. On a normal tty, the characters typed
+/// are echoed. If password entry is desired, use `get_password` or `get_password_with_confirm`.
 pub fn prompt_string(prompt: &str) -> Result<String, io::Error> {
     print!("{}", prompt);
     let mut s = String::new();
