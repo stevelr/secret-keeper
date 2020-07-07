cee8aa27b851b0011a88df6adcb40b9a51dfca2f
steve <dev@somecool.net>
2020-07-07 05:06:09 +0000

    only read secret file if VAULT_TOKEN was undefined

--- examples/hashivault/scripts/list-keys.sh
+++ examples/hashivault/scripts/list-keys.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/../secret.env
+if [ -z "$VAULT_TOKEN" ]; then
+  if [ -f "$DIR/../secret.env" ]; then source $DIR/../secret.env; else
+	  echo "VAULT_TOKEN not defined, and no secret.env"
+	  exit 1
+  fi
+fi
 
 VAULT_ADDR=${VAULT_ADDR:-http://127.0.0.1:8200/}
 
-curl -X LIST -L --silent \
+curl -sS -XLIST -L --silent \
 	--header "X-Vault-Token: $VAULT_TOKEN" \
 	${VAULT_ADDR}v1/transit/keys \
 	| jq ".data"
