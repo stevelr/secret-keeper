0ceda132c594e7c1f8f78f26eaeb5e6767a0597e
steve <dev@somecool.net>
2020-07-07 05:05:51 +0000

    only read secret file if VAULT_TOKEN was undefined

--- examples/hashivault/scripts/make-key.sh
+++ examples/hashivault/scripts/make-key.sh
@@ -3,7 +3,9 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/../secret.env
+if [ -z "$VAULT_TOKEN" ]; then
+  source $DIR/../secret.env
+fi
 
 KEY_NAME="$1"
 if [ -z "$KEY_NAME" ] || [ "$KEY_NAME" = "-h" ] || [ "$KEY_NAME" = "--help" ]; then
@@ -14,7 +16,7 @@ fi
 KEY_TYPE="${KEY_TYPE:-aes256-gcm96}"
 VAULT_ADDR=${VAULT_ADDR:-http://127.0.0.1:8200/}
 
-curl -X POST -L \
+curl -sS -XPOST -L \
 	--header "X-Vault-Token: $VAULT_TOKEN" \
 	--data "{\"type\":\"$KEY_TYPE\",\"exportable\":true,\"allow_plaintext_backup\":true}" \
 	${VAULT_ADDR}v1/transit/keys/$KEY_NAME
