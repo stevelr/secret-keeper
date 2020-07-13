14c572840bae2d58ed76a8e4af83413c3dafc1bf
steve <dev@somecool.net>
2020-07-13 06:51:25 +0000

    add simple example demo

---
+++ examples/simple/src/main.rs
@@ -0,0 +1,38 @@
+use secret_keeper::{
+    error::{Error,Result},
+    keepers::SecretKeeper,
+    ciphers::{CipherKind, xchacha20::NONCEBYTES},
+    rand,
+};
+
+const KEEPER_URI: &str ="env:";
+const PLAINTEXT: &[u8] = b"hello world!";
+const CIPHER: CipherKind = CipherKind::XChaCha20Poly1305;
+
+/// Sample program demonstrating use of SecretKeeper and Cipher apis.
+#[tokio::main]
+async fn main() -> Result<(),Error> {
+    // set password in environment so the example runs cleanly
+    std::env::set_var("VAULT_PASSWORD", "my-secret");
+
+    // Sender
+    let mut nonce = [ 0u8; NONCEBYTES ];
+    rand::fill_buf(&mut nonce)?;
+    let keeper = SecretKeeper::for_uri(KEEPER_URI).await?;
+    let cipher = keeper.init_cipher(CIPHER,&nonce,None).await?;
+    let buf = cipher.seal(PLAINTEXT, None).await?;
+    let envelope = cipher.export(KEEPER_URI, &nonce, &keeper).await?;
+    
+    // envelope contains the keeper_uri and the encrypted key
+    // to decrypt 'buf', a recipient needs envelope and nonce
+    // (and of course, agreement on the cipher algorithm)
+
+    // Reciever
+    let keeper = SecretKeeper::for_uri(&envelope.key_uri).await?;
+    let cipher = keeper.init_cipher(CIPHER, &nonce, Some(&envelope)).await?;
+    let result = cipher.open(&buf, None).await?;
+
+    assert_eq!(&result, PLAINTEXT);
+    println!("Success!");
+    Ok(())
+}
